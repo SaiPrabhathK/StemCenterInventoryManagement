ee17475079df6ff49a1ae4ba339c732b1b3a5c6b
spk6f2 <spk6f2@umsystem.edu>
2023-03-13 16:52:16 -0500

    feat: working on Doc strings

--- globals_.py
+++ globals_.py
@@ -3,8 +3,13 @@
 Created on Thu Jan  5 17:54:34 2023
 
 @author: spk6f2
+
+This module initializes the following objects as a base object type:
+    creds, booksList, flashCardsList and currentStatus.
 """
 
+# The below objects get assigned to appropriate object classes, after
+# InventoryFunctions module gets called in main module.
 creds = object()
 booksList = object()
 flashCardsList = object()
--- main.py
+++ main.py
@@ -3,12 +3,24 @@
 Created on Tue Jan  3 13:53:27 2023
 
 @author: spk6f2
+
+The main driver code that calls functions to initialize globals
+and launches the Main menu by calling callMenu() from InventoryFunctions 
+module
 """
 
 import InventoryFunctions as IF
 
 
 def main():
+    """
+    
+
+    Returns
+    -------
+    None.
+
+    """
     IF.buildCreds()
     IF.retrieveBooksAndFlashCards()
     IF.retrieveCurrentStatus()
